f05a31571006709cdc406db225d12dd75975cc50
Nikolay Cherevan <cherevan.n.s@gmail.com>
2019-12-10 17:46:01 +0200

    Finally production

---
+++ src/Listitems.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function ListItems(props) {
+    const items = props.items;
+    const listItems = items.map(item =>
+        {
+            return <div className="list" key={item.key}>
+                <p>
+                    <input type ="text" id = {item.key}
+                    value = {item.text}
+                    onChange = {
+                        (e) => {
+                            props.setUpdate(e.target.value, item.key)
+                        }
+                    }
+                    />
+                </p>
+                <p 
+                onClick= {() => props.deleteItem(item.key)
+                }
+                
+                > remove </p>
+                
+                 </div>
+        })
+        
+        return (
+            <div> {listItems} </div>
+        )
+}
+export default ListItems;
