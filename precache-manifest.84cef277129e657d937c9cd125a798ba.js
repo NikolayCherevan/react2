d777cbe31be6d44ac96c5093f9ecfbb38b6d8f79
Nikolay Cherevan <cherevan.n.s@gmail.com>
2019-12-10 18:13:52 +0200

    Updates

---
+++ precache-manifest.84cef277129e657d937c9cd125a798ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d91155619c7e37e3027a5d1b8678d2d",
+    "url": "/react2/index.html"
+  },
+  {
+    "revision": "89f0e6b0e7d5032b0ccd",
+    "url": "/react2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "067a37ae256d4c5c99e9",
+    "url": "/react2/static/js/2.f8393227.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react2/static/js/2.f8393227.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89f0e6b0e7d5032b0ccd",
+    "url": "/react2/static/js/main.e5bb743b.chunk.js"
+  },
+  {
+    "revision": "11eb40e76f0fe44a8dd2",
+    "url": "/react2/static/js/runtime-main.4e7516dd.js"
+  }
+]);
